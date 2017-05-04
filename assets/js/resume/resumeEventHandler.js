aa80dac9059a1f95d39df25768a96ca042b616a6
KendrickPC <kennethpchang@gmail.com>
2017-05-04 17:28:45 +0800

    language menu added

--- assets/js/resume/resumeEventHandler.js
+++
@@ -1,7 +0,0 @@
-$(".section-header").click(function () {
-    var sibs = $(this).siblings();
-    var parent = $(this).parent();
-    $(sibs).slideToggle();
-    $(parent).toggleClass("active");
-    $(this).children(".section-icon").toggleClass("active");
-});

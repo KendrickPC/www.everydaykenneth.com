efb782b545874a07d0a39ef32ceb60bd84548961
KendrickPC <kennethpchang@gmail.com>
2017-05-09 15:47:20 +0800

    added timeline.js and worked a rough draft of my about page

---
+++ scripts/typedJSStrings.js
@@ -0,0 +1,6 @@
+  document.addEventListener('DOMContentLoaded', function(){
+      Typed.new('.typedjs', {
+        strings: ["Welcome to EveryDayKenneth.com", "Come check out my About page.", "And browse my portfolio projects.", "I love my work as a full stack developer.", "I also enjoy blogging about tech and code.", "If you want to chat, please contact me. =)"],
+        typeSpeed: 0
+      });
+  });
